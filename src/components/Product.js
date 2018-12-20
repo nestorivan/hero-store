75f90307c0d2770f8289fdb6ecfa0e4175fc0454
Nestor Lopez <nlopezdev@gmail.com>
2018-12-20 03:14:52 -0600

    add handleAddItem to product component

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import numeral from 'numeral';
 
+
 import './product.css';
 
 const Product = ({
-  product: { title, brand, price, description: des, image: img }
+  product: { id, title, brand, price, description: des, image: img },
+  handleAddItem
 }) => {
   return (
-    <div className="col-4 product my-4">
+    <div className="col-sm-4 product my-4 text-center">
       <img className="img-thumbnail mb-3" src={`/img/${img}`} alt={title} />
       <div className="text-center text-muted">{brand}</div>
       <div className="text-center text-dark product-title my-1">{title}</div>
       <div className="text-center text-muted">
-      {numeral(price).format('$0,0.00')}
+        {numeral(price).format('$0,0.00')}
+      </div>
+      <div>
+        <button className="btn solid" onClick={() => handleAddItem(id)}>Add to cart</button>
       </div>
     </div>
   );
--- src/components/ProductsList.js
+++ src/components/ProductsList.js
@@ -1,23 +1,30 @@
 import React, { useContext } from 'react';
-import { ProductsContext } from '../context/ProductsContext';
 import Product from './Product';
+import { ProductsContext } from '../context/ProductsContext';
+import { CartContext } from './../context/CartContext';
+
 
 const ProductsList = () => {
   const { products, loading } = useContext(ProductsContext);
+  const { handleAddItem } = useContext(CartContext);
   return (
-    <div className="row">
-      <div className="container">
-        {loading ? (
-          <span>Loading...</span>
-        ) : (
-          <div className="row">
-            {products.map(product => (
-              <Product product={product} />
-            ))}
+        <div className="row">
+          <div className="container">
+            {loading ? (
+              <span>Loading...</span>
+            ) : (
+              <div className="row">
+                {products.map(product => (
+                  <Product
+                    key={product.id}
+                    product={product}
+                    handleAddItem={handleAddItem}
+                  />
+                ))}
+              </div>
+            )}
           </div>
-        )}
-      </div>
-    </div>
+        </div>
   );
 };
 
