330b82b1f96610874e9273166ce135c09b068d25
Nestor Lopez <nlopezdev@gmail.com>
2018-12-20 03:13:29 -0600

    add cart items count

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,6 +9,7 @@ import {
   NavLink
 } from 'reactstrap';
 import { StoreContext } from '../context/StoreContext';
+import { CartContext } from '../context/CartContext';
 
 import './Header.css';
 
@@ -16,22 +17,30 @@ const Header = () => {
   const [isOpen, toggle] = useState(false);
 
   const { handleToggleCart } = useContext(StoreContext);
+  const { items } = useContext(CartContext);
 
-  //todo: hook cart item count
-  const test = 40;
+  let counter = 0;
+
+  Object.entries(items).forEach(([key, value]) => {
+    counter += value;
+  });
 
   return (
     <div className="header">
       <Navbar color="faded" light expand="md" className="header-navbar">
         <NavbarBrand href="#">
-          <img src="/img/logo.png" alt="hero"></img>
+          <img src="/img/logo.png" alt="hero" />
         </NavbarBrand>
         <NavbarToggler onClick={() => toggle(!isOpen)} />
         <Collapse isOpen={isOpen} navbar>
           <Nav className="ml-auto" navbar>
             <NavItem>
               <NavLink href="#" onClick={handleToggleCart}>
-                {test !== 0 ? <span>My Cart({test})</span> : <span>My Cart</span>}
+                {counter > 0 ? (
+                  <span>My Cart({counter})</span>
+                ) : (
+                  <span>My Cart</span>
+                )}
               </NavLink>
             </NavItem>
           </Nav>
